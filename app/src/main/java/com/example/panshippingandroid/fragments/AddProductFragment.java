513c5af3a361df1c8957a210ccc9c99b4fef8c45
DEVK152\Panonit <adrijana.savic@panonit.com>
2021-07-16 14:18:35 +0200

    Create AddProduct Fragment

---
+++ app/src/main/java/com/example/panshippingandroid/fragments/AddProductFragment.java
@@ -0,0 +1,32 @@
+package com.example.panshippingandroid.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.panshippingandroid.R;
+
+public class AddProductFragment extends Fragment {
+
+    public static final String TAG = "Add product fragment";
+
+    public static AddProductFragment newInstance() {
+        AddProductFragment fragment = new AddProductFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public AddProductFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_add_product, container, false);
+    }
+}
